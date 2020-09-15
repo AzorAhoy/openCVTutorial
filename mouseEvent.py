0a3df50b1c1b95cd63478f5552a14200c7afff0b
anh.tran <anhesrever@gmail.com>
2020-09-15 18:15:07 +0700

    fourth commit

--- mouseEvent.py
+++ mouseEvent.py
@@ -10,20 +10,33 @@ def click_event(event,x,y,flags,params):
         font = cv2.FONT_HERSHEY_SIMPLEX
         strXY = str(x) + ', ' + str(y)
         cv2.putText(img, strXY, (x,y), font, .5, (255, 255, 0), 1)
+
+        cv2.circle(img, (x, y), 3, (0, 0, 255), -1)
+        points.append((x, y))
+        if len(points) >=2:
+            cv2.line(img, points[-1], points[-2], (255, 0, 0), 5)
+
+        # blue = img[x, y, 0]
+        # green = img[x, y, 1]
+        # red = img[x, y, 2]
+        # cv2.circle(img, (x, y), 3, (0, 0, 255), -1)
+        # myColorImage = np.zeros((512, 512, 3), np.uint8)
+        # myColorImage[:] = [blue, green, red]
+
         cv2.imshow('image', img)
-    if event == cv2.EVENT_RBUTTONDOWN:
-        blue = img[y, x, 0]
-        green = img[y, x, 1]
-        red = img[y, x, 2]
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        strBGR = str(blue) + ', ' + str(green) + ', ' + str(red)
-        cv2.putText(img, strBGR, (x,y), font, .5, (0, 255, 255), 1)
-        cv2.imshow('image', img)
+    # if event == cv2.EVENT_RBUTTONDOWN:
+    #     blue = img[y, x, 0]
+    #     green = img[y, x, 1]
+    #     red = img[y, x, 2]
+    #     font = cv2.FONT_HERSHEY_SIMPLEX
+    #     strBGR = str(blue) + ', ' + str(green) + ', ' + str(red)
+    #     cv2.putText(img, strBGR, (x,y), font, .5, (0, 255, 255), 1)
+    #     cv2.imshow('image', img)
 
 # img = np.zeros((512, 512, 3), np.uint8)
-img = cv2.imread('lena.jpg')
+img = cv2.imread('messi5.jpg')
 cv2.imshow('image', img)
-
+points = []
 cv2.setMouseCallback('image',click_event)
 
 cv2.waitKey(0)
